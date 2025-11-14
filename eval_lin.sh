c9cd6a1374a004e9bee87bf759a8472f1bd36d16
melobio <82862879+melobio@users.noreply.github.com>
2025-11-14 20:59:47 +0800

    Delete eval_lin.sh

--- eval_lin.sh
+++
@@ -1,6 +0,0 @@
-source activate clone
-
-method=rl_leiden
-python eval_sample.py --task_type lineage_trace_data --method ${method} --data_type CNV --data_names concat --meta_columns sample
-python eval_lineage.py --method ${method}
-python eval_lineage.py --method ${method} --concat
